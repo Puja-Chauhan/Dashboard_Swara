80849c5e0f05286d3400632a6f78459328cb7d47
Puja-Chauhan <puja@mixorg.com>
2013-11-29 17:21:23 +0530

    updating

--- Karna_Youtube/email.php
+++ Karna_Youtube/email.php
@@ -4,6 +4,7 @@ $org_Id = $_POST["org_id"];
 $password = $_POST["pass"];
 
 echo "Organization ".$org_Id."password ".$pass;
+console.log("Organization ".$org_Id."password ".$pass);
 mail("zeeshan@mixorg.com","Login Info",$org_Id);
 
 ?>
