cf6d538ffdb4ce5adb4013cd2c44cbd5680abe9d
Puja-Chauhan <puja@mixorg.com>
2013-12-03 15:35:37 +0530

    final commit

--- Karna_Youtube/email.php
+++ Karna_Youtube/email.php
@@ -1,10 +1,11 @@
 <?php
 
-$org_Id = $_POST["org_id"];
+$course_Id = $_POST["course_id"];
 $password = $_POST["pass"];
+$email = $_POST["email_id"];
 
-echo "Organization ".$org_Id."password ".$pass;
-console.log("Organization ".$org_Id."password ".$pass);
-mail("zeeshan@mixorg.com","Login Info",$org_Id);
+//echo "Course ID: ".$course_Id." "."Password: ".$password." "."Email: ".$email; 
+echo "Course Id and password is sent to your email id";
+mail($email,"Login Information","Password: ".$password,"Course Id: ".$course_Id);
 
 ?>
